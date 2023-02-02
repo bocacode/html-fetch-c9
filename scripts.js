8ea4d1c33df02d9a859ff6d54e1efab60831c508
Todd Albert <todd@bocacode.com>
2023-02-02 08:27:09 -0500

    added comments in code

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const coffeeList = document.getElementById('coffeeList')
 
+// creates and returns a single coffee: <li>Latte</li>
 function addSingleCoffee(coffee) {
   const newListItem = document.createElement('li')
   const newText = document.createTextNode(coffee.title)
@@ -7,9 +8,11 @@ function addSingleCoffee(coffee) {
   return newListItem
 }
 
+// creates a list <ul></ul>
+// loops through list of coffees and add each one to the list
+// then adds the list to the DOM at #coffeeList
 function newCoffeeList(listOfCoffees) {
   const newList = document.createElement('ul')
-  // loop through list of coffees and add each one to this list
   listOfCoffees.forEach(coffee => {
     const thisItem = addSingleCoffee(coffee)
     newList.appendChild(thisItem)
@@ -18,6 +21,7 @@ function newCoffeeList(listOfCoffees) {
   coffeeList.appendChild(newList)
 }
 
+// fetches coffee list (hot or iced) from Sample APIs
 function getCoffee(type) {
   coffeeList.innerText = "Loading..."
   fetch(`https://api.sampleapis.com/coffee/${type}`)
@@ -29,6 +33,7 @@ function getCoffee(type) {
     .catch(err => console.error(err))
 }
 
+// same as above, but with async..await instead of .then().catch()
 async function getCoffee2() {
   coffeeList.innerText = "Loading...";
   try{
